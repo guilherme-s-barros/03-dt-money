6112a7cc3f552fe2a437aeaaf794bb07352483b3
Guilherme <guilherme.s.barros@outlook.com>
2025-07-18 00:08:20 -0300

    Estrutura visual - Setup do projeto

---
+++ src/@types/styled-components.d.ts
@@ -0,0 +1,15 @@
+import { CSSProp } from 'styled-components'
+
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
+
+declare module "react" {
+  interface DOMAttributes<T> {
+    css?: CSSProp;
+  }
+}
