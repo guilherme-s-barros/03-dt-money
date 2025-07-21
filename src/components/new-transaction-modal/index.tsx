e3f7044ce6dc632a7c2892019566d091950c03a5
Guilherme <guilherme.s.barros@outlook.com>
2025-07-21 20:23:54 -0300

    Estrutura visual - Botões de entrada e saída

--- src/components/new-transaction-modal/index.tsx
+++ src/components/new-transaction-modal/index.tsx
@@ -1,7 +1,7 @@
-import { XIcon } from '@phosphor-icons/react'
+import { ArrowCircleDownIcon, ArrowCircleUpIcon, XIcon } from '@phosphor-icons/react'
 import * as Dialog from '@radix-ui/react-dialog'
 
-import { CloseButton, Content, Overlay } from './styles'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -19,6 +19,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUpIcon size={24} />
+              Entrada
+            </TransactionTypeButton>
+            
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDownIcon size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
--- src/components/new-transaction-modal/styles.ts
+++ src/components/new-transaction-modal/styles.ts
@@ -70,3 +70,35 @@ export const CloseButton = styled(Dialog.Close)`
   padding: 0.5rem;
   cursor: pointer;
 `
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome'
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${props => props.theme['gray-700']};
+  color: ${props => props.theme['gray-300']};
+  padding: 1rem;
+  border: 0;
+  border-radius: 0.5rem;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  cursor: pointer;
+
+  svg {
+    color: ${props => props.variant === 'income'
+      ? props.theme['green-300']
+      : props.theme['red-300']};
+  }
+`
